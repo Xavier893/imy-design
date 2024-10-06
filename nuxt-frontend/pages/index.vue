7723592c00ea121e01ba3b2f0e8aadf46fbbdd6a
XavierReynolds <xavierrynolds@gmail.com>
2024-10-06 19:44:28 +0200

    refactor: Update product descriptions and add product information dialog

--- nuxt-frontend/pages/index.vue
+++ nuxt-frontend/pages/index.vue
@@ -20,6 +20,7 @@ onMounted(() => {
 			price: 789,
 			category: "Scale Cars",
 			inventoryStatus: "OUTOFSTOCK",
+			description: `This collector's item meets the highest standards currently demanded by slot car enthusiasts. It features a finely and authentically printed driver's figure, with each figure individually painted to match the specific vehicle. The car is compatible with all commercial track systems and comes fully assembled. Additionally, it is presented on a removable display base, protected by a clear acrylic dust cover, ensuring both functionality and elegant display.`,
 			rating: 4.7,
 		},
 		{
@@ -28,6 +29,7 @@ onMounted(() => {
 			image: "/prods/barrels.jpg",
 			price: 149,
 			category: "Scale Parts",
+			description: `Tyco chassis use brushes to make contact with the armature to allow it to turn. Though not often, there are times when the brush barrel(s) overheats and hits its melting point. This results in the requirement to replace the barrel that houses the brush.`,
 			inventoryStatus: "INSTOCK",
 			rating: 4.9,
 		},
@@ -36,6 +38,7 @@ onMounted(() => {
 			name: "1989 Batmobile",
 			image: "/prods/batmobile.jpg",
 			price: 1749,
+			description: `The 1989 Batmobile is equipped with a range of advanced features, including Spark Plug compatibility and Digital Plug readiness, making it adaptable to various systems. Its high-definition design is enhanced by Magnatraction, providing superior grip on the track. The Batmobile also boasts flashing lights, working rear lights, and functioning headlights, adding to its authenticity. Additionally, it comes with an easy-change guide blade, allowing for quick and effortless maintenance.`,
 			category: "Scale Cars",
 			inventoryStatus: "LOWSTOCK",
 			rating: 4.4,
@@ -45,6 +48,7 @@ onMounted(() => {
 			name: "Billet T Boss",
 			image: "/prods/tboss.jpg",
 			price: 89,
+			description: `Billet Delrin T boss for all slot cars. This T boss replaces worn and wobbly stock gear bosses. The precision CNC machined T boss keeps gear mesh true and consistent resulting in faster lap times, Less wear and tear and cooler running temps.`,
 			category: "Scale Parts",
 			inventoryStatus: "INSTOCK",
 			rating: 4.2,
@@ -54,6 +58,7 @@ onMounted(() => {
 			name: "Audi R8 HH Police Car",
 			image: "/prods/police.jpg",
 			price: 999,
+			description: `While not your average police car, the Audi R8 is surely something not many criminals would be able to escape from! This Scalextric version comes complete with both a siren and flashing police lights on the roof. Perfect for any aspiring law enforcement officer to test their mettle!`,
 			category: "Scale Cars",
 			inventoryStatus: "INSTOCK",
 			rating: 4.1,
@@ -147,6 +152,19 @@ const responsiveOptions = ref([
 		numScroll: 1,
 	},
 ]);
+
+const featuredVisible = ref(false);
+const animationDuration = ref(3000);
+
+watch(visible, () => {
+	if (visible.value) {
+		animationDuration.value = 0;
+		console.log("Animation Duration:", animationDuration.value);
+	} else {
+		animationDuration.value = 3000;
+		console.log("Animation Duration:", animationDuration.value);
+	}
+});
 </script>
 
 <template>
@@ -159,11 +177,11 @@ const responsiveOptions = ref([
 			:responsiveOptions="responsiveOptions"
 			:numScroll="1"
 			circular
-			:autoplayInterval="3000"
 		>
 			<template #item="slotProps">
 				<div
-					class="border border-surface-200 dark:border-surface-700 bg-white rounded m-2 p-4"
+					class="border border-surface-200 dark:border-surface-700 bg-white cursor-pointer rounded m-2 p-4 hover:bg-white hover:border-2 hover:border-gray-500 hover:transform hover:scale-[1.03] transition-transform"
+					@click="handleProductClick(slotProps.data)"
 				>
 					<div class="mb-4 w-full">
 						<div class="relative mx-auto">
@@ -200,6 +218,52 @@ const responsiveOptions = ref([
 						</span>
 					</div>
 				</div>
+
+				<Dialog
+					v-model:visible="featuredVisible"
+					modal
+					dismissableMask
+					header="Product Information"
+					class="w-[35rem]"
+					:style="{ 'font-family': 'Montserrat' }"
+				>
+					<div class="flex flex-col gap-4 justify-between items-start">
+						<div class="w-full h-[200px]">
+							<img
+								class="rounded w-full h-full object-contain"
+								:src="`${slotProps.data.image}`"
+								:alt="slotProps.data.name"
+							/>
+						</div>
+
+						<div class="flex flex-row w-full justify-between items-start gap-2">
+							<div>
+								<div class="text-lg font-medium mt-1">
+									{{ slotProps.data.name }}
+								</div>
+								<span class="font-medium text-gray-500 text-sm">{{
+									slotProps.data.category
+								}}</span>
+							</div>
+							<div class="bg-surface-100 p-1" style="border-radius: 30px">
+								<div
+									class="bg-surface-0 rounded border-gray-300 border flex items-center gap-2 justify-center py-1 px-2"
+								>
+									<span class="text-surface-900 font-medium text-sm">{{
+										slotProps.data.rating
+									}}</span>
+									<i class="pi pi-star-fill text-yellow-500"></i>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<p>
+								{{ slotProps.data.description }}
+							</p>
+						</div>
+					</div>
+				</Dialog>
 			</template>
 		</Carousel>
 
